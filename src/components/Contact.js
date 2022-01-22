535db7fee64b6eb6618ef28aaea1ce6165c6b543
Ferrois <ferrois2005@gmail.com>
2022-01-23 00:20:45 +0800

    commit3

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -26,7 +26,9 @@ export default class Contact extends Component {
         </div>
 
         <div className='socialContainer'>
-        
+          <div className='socialWrapper1'>Contact Via Social Media.</div>
+          <InstagramIcon/>
+          <YoutubeIcon/>
         </div>
 
       </div>
@@ -84,3 +86,55 @@ class ContactForm extends Component {
     )
   }
 }
+ class InstagramIcon extends Component {
+  componentDidMount(){
+    var elemArr = document.querySelectorAll(".socialIcon");
+    var observer = new IntersectionObserver(function(entries){
+      entries.forEach(entry => {
+        entry.target.classList.toggle("socialIconShow", entry.isIntersecting);
+      })
+    },{threshold:0})
+
+    elemArr.forEach((elem)=>{
+      observer.observe(elem)
+    })
+  }
+
+
+
+  render() {
+    return(
+      <div className="socialWrapper2">
+        <i className="fab fa-5x socialIcon fa-instagram"></i>
+        <p>Follow me on Instagram. If you would like to contact me, you can also contact me via instagram.<br/><a href='https://www.instagram.com/ferroisss/?utm_medium=copy_link'>Link to Instagram</a></p>
+      </div>
+    )
+  }
+}
+
+class YoutubeIcon extends Component {
+  componentDidMount(){
+    var elemArr = document.querySelectorAll(".socialIcon");
+    var observer = new IntersectionObserver(function(entries){
+      entries.forEach(entry => {
+        entry.target.classList.toggle("socialIconShow", entry.isIntersecting);
+      })
+    },{threshold:0.15})
+
+    elemArr.forEach((elem)=>{
+      observer.observe(elem)
+    })
+  }
+
+
+
+  render() {
+    return(
+      <div className="socialWrapper2">
+        <i className="fab fa-5x socialIcon fa-youtube"></i>
+        <p>Subscribe to me on Youtube. If you would like to contact me, you can also contact me via youtube.<br/><a href='https://www.youtube.com/channel/UCv7_eNTdjGMiQ2bCduHpn_Q'>Link to Youtube</a></p>
+      </div>
+    )
+  }
+}
+
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+import "../components-css/Footer.css"
 
 export default class Footer extends Component {
-  render() {
-    return <div></div>;
-  }
+    render() {
+        return(
+            <div className='footerContainer'>
+                <div className='footerSubContainer'>
+                    <div className='footerHeading'>Information</div>
+                    <p>©Copyrights Ferrois</p>
+                    <p>This web-app was made by Ferrois using ReactJS</p>
+                    <div className='footerSiteMap'>
+                        <p>Site Map</p>
+                        <Link to={"/"} className='footerLink'>.home()</Link>
+                        <Link to={"/"} className='footerLink'>.home()</Link>
+                        <Link to={"/"} className='footerLink'>.home()</Link>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
